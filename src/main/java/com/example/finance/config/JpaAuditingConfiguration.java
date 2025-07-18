9dd14faf34c71f2295c0068492a140cb0aee87ae
umar <abduxalil_raxmanberdiyev@uzinfocom.local>
2025-07-18 19:36:40 +0500

    auditing if needed

---
+++ src/main/java/com/example/finance/config/JpaAuditingConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.finance.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@Configuration
+//@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+public class JpaAuditingConfiguration {
+//    @Bean
+//    public AuditorAware<String> auditorProvider() {
+//        return () -> {
+//            Optional<String> username;
+//            if (SecurityContextHolder.getContext().getAuthentication() == null) {
+//                username = Optional.of("admin");
+//            } else
+//                username = Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication().getName());
+//            return username;
+//        };
+//    }
+}
+
---
+++ src/main/java/com/example/finance/infratructure/persistance/model/Transaction.java
@@ -0,0 +1,38 @@
+package com.example.finance.infratructure.persistance.model;
+
+import com.example.finance.domain.base.Auditable;
+import com.example.finance.domain.enums.TransactionStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+import static jakarta.persistence.GenerationType.SEQUENCE;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class Transaction extends Auditable<String> {
+    @Id
+    @GeneratedValue(strategy = SEQUENCE, generator = "USER_SEQUENCE")
+    @SequenceGenerator(sequenceName = "USER_SEQUENCE", allocationSize = 1, name = "USER_SEQUENCE")
+    @Column(nullable = false, updatable = false)
+    private Long id;
+
+    @Column
+    private String comment;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionStatus status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Card sourceCard;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Card destinationCard;
+}

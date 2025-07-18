27a1a3117187051bebaca6fa3e02fbace0b6f00f
umar <abduxalil_raxmanberdiyev@uzinfocom.local>
2025-07-18 19:43:25 +0500

    fix sequence

--- src/main/java/com/example/finance/infratructure/persistance/model/Transaction.java
+++ src/main/java/com/example/finance/infratructure/persistance/model/Transaction.java
@@ -16,8 +16,8 @@ import static jakarta.persistence.GenerationType.SEQUENCE;
 @NoArgsConstructor
 public class Transaction extends Auditable<String> {
     @Id
-    @GeneratedValue(strategy = SEQUENCE, generator = "USER_SEQUENCE")
-    @SequenceGenerator(sequenceName = "USER_SEQUENCE", allocationSize = 1, name = "USER_SEQUENCE")
+    @GeneratedValue(strategy = SEQUENCE, generator = "TRANSACTION_SEQUENCE")
+    @SequenceGenerator(sequenceName = "TRANSACTION_SEQUENCE", allocationSize = 1, name = "TRANSACTION_SEQUENCE")
     @Column(nullable = false, updatable = false)
     private Long id;
 

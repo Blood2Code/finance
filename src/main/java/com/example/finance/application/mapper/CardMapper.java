8d35ba10746a73d1e495b102ddadcac684b6d558
umar <abduxalil_raxmanberdiyev@uzinfocom.local>
2025-07-18 19:51:15 +0500

    unused data

--- src/main/java/com/example/finance/application/mapper/CardMapper.java
+++ src/main/java/com/example/finance/application/mapper/CardMapper.java
@@ -1,9 +1,7 @@
 package com.example.finance.application.mapper;
 
 import com.example.finance.application.dto.CardDto;
-import com.example.finance.application.dto.UserDto;
 import com.example.finance.infratructure.persistance.model.Card;
-import com.example.finance.infratructure.persistance.model.User;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
